43b6e97f13d46d233ab094f5148d593713817365
Mick Watson <mick.watson@roslin.ed.ac.uk>
2017-10-16 09:47:17 +0100

    Update megahit.sh

--- shells/megahit.sh
+++ shells/megahit.sh
@@ -2,7 +2,4 @@
 
 source $HOME/.bashrc
 
-date >> /exports/cmvm/eddie/eb/groups/watson_grp/snakemake_assembly/shells/mgruns.txt
-echo $@ >> /exports/cmvm/eddie/eb/groups/watson_grp/snakemake_assembly/shells/mgruns.txt
-
 /exports/cmvm/eddie/eb/groups/watson_grp/software/megahit/megahit $@
